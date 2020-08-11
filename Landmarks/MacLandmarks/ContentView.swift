a7a6b7c19680b0a433e31a1bd1ed3c3286887e46
Phillip LeMaster <philalem21@gmail.com>
2020-08-10 21:14:32 -0400

    completed the app

--- Landmarks/MacLandmarks/ContentView.swift
+++ Landmarks/MacLandmarks/ContentView.swift
@@ -9,15 +9,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selectedLandmark: Landmark?
+    
+    
     var body: some View {
-        Text("Hello, World!")
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
+        NavigationView {
+            NavigationPrimary(selectedLandmark: $selectedLandmark)
+            
+            if selectedLandmark != nil {
+                NavigationDetail(landmark: selectedLandmark!)
+            }
+        }.frame(minWidth: 700, minHeight: 300)
     }
 }
 
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().environmentObject(UserData())
     }
 }
